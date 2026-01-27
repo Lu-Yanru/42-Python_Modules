1cae9b06bacf123f351a10375e79406f4c929782
Yanru Lu <yanlu@c3a2c10.42berlin.de>
2026-01-27 12:29:05 +0100

    modify ex2 to not use loop

--- 00/ex2/ft_harvest_total.py
+++ 00/ex2/ft_harvest_total.py
@@ -1,6 +1,9 @@
 def ft_harvest_total():
     total = 0
-    for i in range(1, 4):
-        weight = int(input(f"Day {i} harvet: "))
-        total += weight
+    weight1 = int(input("Day 1 harvest: "))
+    total += weight1
+    weight2 = int(input("Day 2 harvest: "))
+    total += weight2
+    weight3 = int(input("Day 3 harvest: "))
+    total += weight3
     print(f"Total harvest: {total}")
