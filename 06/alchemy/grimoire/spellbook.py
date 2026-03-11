d86c62f005d38fb00ea03141e26fe87f8a023896
Yanru Lu <yanlu@c3a1c6.42berlin.de>
2026-03-11 12:36:40 +0100

    add circular_curse

---
+++ 06/alchemy/grimoire/spellbook.py
@@ -0,0 +1,7 @@
+def record_spell(spell_name: str, ingredients: str) -> str:
+    from .validator import validate_ingredients
+
+    res = validate_ingredients(ingredients)
+    if res.endswith("VALID"):
+        return f"Spell recorded: {spell_name} ({res})"
+    return f"Spell rejected: {spell_name} ({res})"
---
+++ 06/alchemy/grimoire/validator.py
@@ -0,0 +1,9 @@
+# from .spellbook import record_spell
+
+
+def validate_ingredients(ingredients: str) -> str:
+    valid_ingredients = ["fire", "water", "earth", "air"]
+    for word in ingredients.split():
+        if word not in valid_ingredients:
+            return f"{ingredients} - INVALID"
+    return f"{ingredients} - VALID"
